31919f84fe018e87f6472426e241610e98ac4a90
faddey <no@No.no>
2021-12-20 17:57:33 +0300

    correct mistake and print needs [air numbers

--- main.cpp
+++ main.cpp
@@ -6,13 +6,19 @@
     void max_sum_div()
     {
         int n(0);
+        int v(0);
         cin >> n;
         vector<int> numbers(n, 0);
         for (int i(0); i<n; i++)
             cin >> numbers[i];
         for (int i(0); i<n; i++)
         for (int j(i+1); j<n; j++)
-            cout << numbers[i]+numbers[j]<< endl;
+        {
+            v=numbers[i]*numbers[j];
+            if (v%35==0)
+                cout << v<< endl;
+        }
+
 
     }
     int main()
