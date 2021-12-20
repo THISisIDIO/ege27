a55fb2b949cd09198b2486c2dcc9c7a69861cf13
faddey <no@No.no>
2021-12-20 18:17:36 +0300

    make effective solution

--- main.cpp
+++ main.cpp
@@ -6,21 +6,26 @@
     void max_sum_div()
     {
         int n(0);
-        int v(0);
-        int q(0);
+        int n5(0);
+        int n7(0);
+        int n0(0);
+        int n35(0);
         cin >> n;
         vector<int> numbers(n, 0);
         for (int i(0); i<n; i++)
             cin >> numbers[i];
         for (int i(0); i<n; i++)
-        for (int j(i+1); j<n; j++)
         {
-            v=numbers[i]*numbers[j];
-            if (v%35==0)
-                q+=1;
-
+            if (numbers[i]%5==0 and numbers[i]%35!=0)
+                n5+=1;
+            if (numbers[i]%7==0 and numbers[i]%35!=0)
+                n7+=1;
+            if (numbers[i]%7!=0 and numbers[i]%5!=0)
+                n0+=1;
+            if (numbers[i]%35==0)
+                n35+=1;
         }
-        cout << q;
+        cout << n5*n7+n35*n5+n7*n35+n0*n35+(n35*(n35-1))/2;
 
 
     }
