3dd94dc1050664b7fec85987180df3b11ccf4cf7
faddey <no@No.no>
2021-12-21 12:34:10 +0300

    print numbers

--- main.cpp
+++ main.cpp
@@ -6,28 +6,12 @@
     void max_sum_div()
     {
         int n(0);
-        int n5(0);
-        int n7(0);
-        int n0(0);
-        int n35(0);
         cin >> n;
         vector<int> numbers(n, 0);
         for (int i(0); i<n; i++)
             cin >> numbers[i];
         for (int i(0); i<n; i++)
-        {
-            if (numbers[i]%5==0 and numbers[i]%35!=0)
-                n5+=1;
-            if (numbers[i]%22==0 and numbers[i]%35!=0)
-                n7+=1;
-            if (numbers[i]%7!=0 and numbers[i]%5!=0)
-                n0+=1;
-            if (numbers[i]%35==0)
-                n35+=1;
-        }
-        cout << n5*n7+n35*n5+n7*n35+n0*n35+(n35*(n35-1))/2;
-
-
+            cout << numbers[i];
     }
     int main()
 {
