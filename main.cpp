c1984f806077d91a48b01e31eef9172066d4b968
faddey <no@No.no>
2021-12-21 13:23:23 +0300

    print true trio and min product

--- main.cpp
+++ main.cpp
@@ -6,7 +6,10 @@
     void max_sum_div()
     {
         int n(0);
-        int k(0);
+        int z(0);
+        int x(0);
+        int y(0);
+        int vo=99999;
         cin >> n;
         vector<int> numbers(n, 0);
         for (int i(0); i<n; i++)
@@ -15,11 +18,20 @@
         for (int j(i+1); j<n; j++)
         for (int p(j+1); p<n; p++)
         {
-            if ((numbers[i]*numbers[j]*numbers[p])%4!=0)
-                k+=1;
-        }
-        cout << k;
+            int vod=numbers[i]*numbers[j]*numbers[p];
+            if (vod%4!=0)
+            {
+                x=i;
+                y=j;
+                z=p;
+                if (vod<vo)
+                    vo=vod;
+            }
 
+        }
+
+        cout << x << " "<< y << " " << z << endl;
+        cout << vo;
     }
     int main()
 {
